e9122ddf373b84d7be40a1f6a21bddf3b402659d
Bergogliox <Stefano300804@gmail.com>
2025-03-24 18:17:11 +0100

    Stef files

---
+++ src/main/java/labioopint/controller/api/DirectionCheck.java
@@ -0,0 +1,122 @@
+package labioopint.controller.api;
+
+import labioopint.model.api.Coordinate;
+import labioopint.model.maze.api.BlockType;
+import labioopint.model.maze.api.Rotation;
+import labioopint.model.maze.impl.Block;
+import labioopint.model.maze.impl.Labyrinth;
+import labioopint.model.maze.impl.Maze;
+
+public class DirectionCheck {
+    /*
+     * Every functions check if there are an entrance
+     */
+    public static boolean checkRightEntrance(Labyrinth lab, Coordinate coord){
+        Maze grid = lab.getGrid();
+        Block block = grid.GetBlock(coord);
+        BlockType BType = block.getType();
+        Rotation rotation = block.getRotation();
+
+        switch (BType) {
+            case BlockType.CORNER:
+                if(rotation == Rotation.ZERO || rotation == Rotation.TWO_HUNDRED_SEVENTY){
+                    return true;
+                }
+                break;
+            case BlockType.CORRIDOR:
+                if(rotation == Rotation.NINETY || rotation == Rotation.TWO_HUNDRED_SEVENTY){
+                    return true;
+                }
+                break;
+            case BlockType.CROSSING:
+                if(rotation == Rotation.ZERO || rotation == Rotation.ONE_HUNDRED_EIGHTY || rotation == Rotation.TWO_HUNDRED_SEVENTY){
+                    return true;
+                }
+                break;
+            default:
+                break;
+        }
+        return false;
+    }
+    public static boolean checkLeftEntrance(Labyrinth lab, Coordinate coord){
+        Maze grid = lab.getGrid();
+        Block block = grid.GetBlock(coord);
+        BlockType BType = block.getType();
+        Rotation rotation = block.getRotation();
+
+        switch (BType) {
+            case BlockType.CORNER:
+                if(rotation == Rotation.NINETY || rotation == Rotation.ONE_HUNDRED_EIGHTY){
+                    return true;
+                }
+                break;
+            case BlockType.CORRIDOR:
+                if(rotation == Rotation.NINETY || rotation == Rotation.TWO_HUNDRED_SEVENTY){
+                    return true;
+                }
+                break;
+            case BlockType.CROSSING:
+                if(rotation == Rotation.ZERO || rotation == Rotation.ONE_HUNDRED_EIGHTY || rotation == Rotation.NINETY){
+                    return true;
+                }
+                break;
+            default:
+                break;
+        }
+        return false;
+    }
+    public static boolean checkBottomEntrance(Labyrinth lab, Coordinate coord){
+        Maze grid = lab.getGrid();
+        Block block = grid.GetBlock(coord);
+        BlockType BType = block.getType();
+        Rotation rotation = block.getRotation();
+
+        switch (BType) {
+            case BlockType.CORNER:
+                if(rotation == Rotation.ZERO || rotation == Rotation.NINETY){
+                    return true;
+                }
+                break;
+            case BlockType.CORRIDOR:
+                if(rotation == Rotation.ZERO || rotation == Rotation.ONE_HUNDRED_EIGHTY){
+                    return true;
+                }
+                break;
+            case BlockType.CROSSING:
+                if(rotation == Rotation.ZERO || rotation == Rotation.NINETY || rotation == Rotation.TWO_HUNDRED_SEVENTY){
+                    return true;
+                }
+                break;
+            default:
+                break;
+        }
+        return false;
+    }
+    public static boolean checkUpperEntrance(Labyrinth lab, Coordinate coord){
+        Maze grid = lab.getGrid();
+        Block block = grid.GetBlock(coord);
+        BlockType BType = block.getType();
+        Rotation rotation = block.getRotation();
+
+        switch (BType) {
+            case BlockType.CORNER:
+                if(rotation == Rotation.ONE_HUNDRED_EIGHTY || rotation == Rotation.TWO_HUNDRED_SEVENTY){
+                    return true;
+                }
+                break;
+            case BlockType.CORRIDOR:
+                if(rotation == Rotation.ZERO || rotation == Rotation.ONE_HUNDRED_EIGHTY){
+                    return true;
+                }
+                break;
+            case BlockType.CROSSING:
+                if(rotation == Rotation.ONE_HUNDRED_EIGHTY || rotation == Rotation.NINETY || rotation == Rotation.TWO_HUNDRED_SEVENTY){
+                    return true;
+                }
+                break;
+            default:
+                break;
+        }
+        return false;
+    }
+}
---
+++ src/main/java/labioopint/model/api/ActionType.java
@@ -0,0 +1,10 @@
+package labioopint.model.api;
+
+public enum ActionType{
+    GAME_LOARDING,
+    SETTINGS,
+    SAVING,
+    MOVE_PLAYER,
+    MOVE_BLOCK,
+    ENEMY_TURN
+}
