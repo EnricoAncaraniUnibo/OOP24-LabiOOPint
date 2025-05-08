82d27f8c65b34403b1b1de989a7098e23159093b
Simollino <simone.alocchi@studio.unibo.it>
2025-05-08 17:40:05 +0200

    aggiunta costruttori powerup

--- src/main/java/labioopint/model/powerup/impl/CrossWallPowerUp.java
+++ src/main/java/labioopint/model/powerup/impl/CrossWallPowerUp.java
@@ -3,6 +3,12 @@ package labioopint.model.powerup.impl;
 import labioopint.model.player.impl.PlayerImpl;
 
 public class CrossWallPowerUp extends PowerUpImpl {
+
+    public CrossWallPowerUp() {
+        super();
+        super.setName("Cross Wall");
+    }
+    
     @Override
     public void activate(PlayerImpl currentPlayer) {
         if (isCollected()) {
