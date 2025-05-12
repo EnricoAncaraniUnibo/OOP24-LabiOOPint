5fd92d301904deed2bdc984c629024b845bf19a2
emme004 <andrea.monti24@studio.unibo.it>
2025-05-12 17:44:19 +0200

    checkstyle_test

--- src/test/java/labioopint/labyrinth/BlocksTest.java
+++ src/test/java/labioopint/labyrinth/BlocksTest.java
@@ -8,34 +8,36 @@ import org.junit.jupiter.api.Test;
 import labioopint.model.block.api.BlockType;
 import labioopint.model.block.api.Rotation;
 import labioopint.model.block.impl.BlockImpl;
+
 /**
  * The BlocksTest class contains unit tests for the BlockImpl class.
  * It verifies the behavior of block rotation and ensures that the rotation
  * can be changed correctly.
  */
 class BlocksTest {
+    	
 	/**
-     * A static instance of BlockImpl used for testing.
-     */
+	 * A static instance of BlockImpl used for testing.
+	 */
 	private static BlockImpl b;
-	
+
 	/**
-     * Initializes the BlockImpl instance before all tests are executed.
-     * Sets the block type to CORRIDOR.
-     */
+	 * Initializes the BlockImpl instance before all tests are executed.
+	 * Sets the block type to CORRIDOR.
+	 */
 	@BeforeAll
 	static void init() {
 		b = new BlockImpl(BlockType.CORRIDOR);
 	}
-	
+
 	/**
-     * Tests the setRotation method of BlockImpl.
-     * Ensures that the rotation of the block changes when set to a new value.
-     */
+	 * Tests the setRotation method of BlockImpl.
+	 * Ensures that the rotation of the block changes when set to a new value.
+	 */
 	@Test
 	void changeRotation() {
 		final Rotation r = b.getRotation();
 		b.setRotation(Rotation.ONE_HUNDRED_EIGHTY);
-		assertNotEquals(r,b.getRotation());
+		assertNotEquals(r, b.getRotation());
 	}
 }
